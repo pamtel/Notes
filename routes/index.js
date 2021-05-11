876a5385e156710bec8b585e07ee18caaa0303fa
pamtel <pamelaakosahiante@gmail.com>
2021-05-11 13:19:13 +0100

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
-import express from 'express';
-
+import * as express from 'express';
+import { NotesStore as notes } from "../app.js";
 export const router = express.Router(); 
-
 router.get('/', async (req, res, next) => {
-    res.render('index', { title: 'Notes' });
+    try {
+        const keylist = await notes.keylist();
+        const keyPromises = keylist.map((key) => {
+            return notes.read(key);
+        });
+        const noteslist = await Promise.all(keyPromises);
+        res.render("index", { title: "Tech Academy Notes App"})
+    } catch (err) {
+        next(err)
+    }
 });
